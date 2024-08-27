9339f6bd19ff5bb7c524aeb91c2d16806d40b76d
TejasKathuria <kathuriatectejas08@gmail.com>
2024-08-27 17:11:34 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "public/index.html",
+    "src/App.css",
+    "src/App.css",
+    "src/App.test.js",
+    "src/index.css",
+    "src/index.js",
+    "src/logo.svg",
+    "src/reportWebVitals.js",
+    "src/setupTests.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
